73c284f05b3ac5f7b8a3ef89906b277c918a6d2d
amitabha652 <amitabha.87.work@gmail.com>
2018-11-18 09:58:58 +0530

    18 - Spring Assignment - 4 JAVA - Initial Done

---
+++ 18 - Spring Assignment - 4 JAVA/Assignment ID-10_Spring_Assignment4_output/FSDSpringProject/src/main/java/com/cts/fsd/controller/UtilController.java
@@ -0,0 +1,26 @@
+package com.cts.fsd.controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@RestController
+@CrossOrigin("*")
+public class UtilController {
+ 
+	/************************* API METHODS START *************************/
+	@RequestMapping(value={"", "/", "/welcome", "/home"})
+	public ModelAndView showHomePage() {
+		System.out.println("ApplicationController :: inside showHomePage()");
+		ModelAndView modelViewObject = new ModelAndView("home");
+		return modelViewObject;
+	}
+	
+	@RequestMapping(value={"/error", "/notfound"})
+	public ModelAndView showNotFoundPage() {
+		System.out.println("ApplicationController :: inside showNotFoundPage()");
+		ModelAndView modelViewObject = new ModelAndView("error");
+		return modelViewObject;
+	}
+	
+}
